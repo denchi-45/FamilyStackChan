12d54a564919a5417d8e2cb783cb3a8c62458de5
ndenchi <hori.takoash45@gmail.com>
2024-06-11 23:07:21 +0900

    サーボの動きを追加

--- M5Unified-Stackchan-Population-Control/src/main.cpp
+++ M5Unified-Stackchan-Population-Control/src/main.cpp
@@ -1,18 +1,136 @@
-#include <Arduino.h>
+#include <M5Unified.h>
+#include <Avatar.h>
+#include <ServoEasing.hpp> // https://github.com/ArminJo/ServoEasing 
 
-// put function declarations here:
-int myFunction(int, int);
+#define USE_SERVO
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+using namespace m5avatar;
+
+Avatar avatar;
+
+#define USE_SERVO
+#ifdef USE_SERVO
+#if defined(ARDUINO_M5STACK_Core2)
+//  #define SERVO_PIN_X 13  //Core2 PORT C
+//  #define SERVO_PIN_Y 14
+  #define SERVO_PIN_X 33  //Core2 PORT A
+  #define SERVO_PIN_Y 32
+#elif defined( ARDUINO_M5STACK_FIRE )
+  #define SERVO_PIN_X 21
+  #define SERVO_PIN_Y 22
+#elif defined( ARDUINO_M5Stack_Core_ESP32 )
+  #define SERVO_PIN_X 21
+  #define SERVO_PIN_Y 22
+#endif
+#endif
+
+#ifdef USE_SERVO
+#define START_DEGREE_VALUE_X 100
+//#define START_DEGREE_VALUE_Y 90
+#define START_DEGREE_VALUE_Y 90 //
+ServoEasing servo_x;
+ServoEasing servo_y;
+#endif
+
+bool servo_home = false;
+
+void servo(void *args)
+{
+  float gazeX, gazeY;
+  DriveContext *ctx = (DriveContext *)args;
+  Avatar *avatar = ctx->getAvatar();
+  for (;;)
+  {
+#ifdef USE_SERVO
+    if(!servo_home)
+    {
+    avatar->getGaze(&gazeY, &gazeX);
+    servo_x.setEaseTo(START_DEGREE_VALUE_X + (int)(30.0 * gazeX));
+    if(gazeY < 0) {
+      int tmp = (int)(10.0 * gazeY);
+      if(tmp > 10) tmp = 10;
+      servo_y.setEaseTo(START_DEGREE_VALUE_Y + tmp);
+    } else {
+      servo_y.setEaseTo(START_DEGREE_VALUE_Y + (int)(20.0 * gazeY));
+    }
+    } else {
+//     avatar->setRotation(gazeX * 5);
+//     float b = avatar->getBreath();
+       servo_x.setEaseTo(START_DEGREE_VALUE_X); 
+//     servo_y.setEaseTo(START_DEGREE_VALUE_Y + b * 5);
+       servo_y.setEaseTo(START_DEGREE_VALUE_Y);
+    }
+    synchronizeAllServosStartAndWaitForAllServosToStop();
+#endif
+    delay(50);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void Servo_setup() {
+#ifdef USE_SERVO
+  if (servo_x.attach(SERVO_PIN_X, START_DEGREE_VALUE_X, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE)) {
+    Serial.print("Error attaching servo x");
+  }
+  if (servo_y.attach(SERVO_PIN_Y, START_DEGREE_VALUE_Y, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE)) {
+    Serial.print("Error attaching servo y");
+  }
+  servo_x.setEasingType(EASE_QUADRATIC_IN_OUT);
+  servo_y.setEasingType(EASE_QUADRATIC_IN_OUT);
+  setSpeedForAllServos(30);
+
+  servo_x.setEaseTo(START_DEGREE_VALUE_X); 
+  servo_y.setEaseTo(START_DEGREE_VALUE_Y);
+  synchronizeAllServosStartAndWaitForAllServosToStop();
+#endif
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+struct box_t
+{
+  int x;
+  int y;
+  int w;
+  int h;
+  int touch_id = -1;
+
+  void setupBox(int x, int y, int w, int h) {
+    this->x = x;
+    this->y = y;
+    this->w = w;
+    this->h = h;
+  }
+  bool contain(int x, int y)
+  {
+    return this->x <= x && x < (this->x + this->w)
+        && this->y <= y && y < (this->y + this->h);
+  }
+};
+static box_t box_servo;
+
+void setup()
+{
+
+  auto cfg = M5.config();
+
+  M5.begin(cfg);
+
+  avatar.init(); // start drawing
+
+  // adjust position
+  const auto r = avatar.getFace()->getBoundingRect();
+  const auto scale_w = M5.Display.width() / (float)r->getWidth();
+  const auto scale_h = M5.Display.height() / (float)r->getHeight();
+  const auto scale = std::min(scale_w, scale_h);
+  avatar.setScale(scale);
+  const auto offs_x = (r->getWidth() - M5.Display.width()) / 2;
+  const auto offs_y = (r->getHeight() - M5.Display.height()) / 2;
+  avatar.setPosition(-offs_y, -offs_x);
+
+  Servo_setup();
+  avatar.addTask(servo, "servo");
+}
+
+void loop()
+{
+  // avatar's face updates in another thread
+  // so no need to loop-by-loop rendering
 }
